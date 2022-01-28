40f989343b2c12a9a61221d9c5e6629267ad72fb
moinuddin kamal <cpimoinuddin@gmail.com>
2022-01-29 01:38:54 +0600

    Api Updated

---
+++ FackData.js
@@ -0,0 +1,178 @@
+import React from 'react';
+
+const FackData = () => {
+
+
+    const Prodcuts= [
+        {
+            id=1,
+            name:'Man Shirt',
+            catagory:'Man',
+            price:30,
+            image:'https://i.ibb.co/HG7NW5y/Man-Shirt.jpg',
+            description:'Essentials Mens Regular-fit Long-Sleeve Flannel Shirt'
+        }
+        ,
+        
+        {
+            id=2,
+            name:'Man TShirt',
+            catagory:'Man',
+            price:10,
+            image:'https://i.ibb.co/XWdnVp6/man-Tshart.jpg',
+            description:'Under Armour Mens Tech 2.0 Short-Sleeve T-Shirt'
+        }
+        ,
+        {
+            id=3,
+            name:'Man Pant',
+            catagory:'Man',
+            price:30,
+            image:'https://i.ibb.co/9hRJdv4/ManPants.jpg',
+            description:'Dickies Mens Loose Fit Double Knee Work Pant'
+        }
+        ,
+        {
+            id=4,
+            name:'Man Watch',
+            catagory:'Man',
+            price:30,
+            image:'https://i.ibb.co/TLphjZh/T065-407-11-051-00.jpg',
+            description:'Fossil Mens Grant Stainless Steel Quartz Chronograph Watch'
+        }
+        ,
+        {
+            id=5,
+            name:'Man Sunglas',
+            catagory:'Man',
+            price:30,
+            image:'https://i.ibb.co/64jPC0f/man-Sunglass.jpg',
+            description:'Mens Sports Polarized Sunglasses Square Frame Glasses'
+        }
+        ,
+        {
+            id=6,
+            name:'Man show',
+            catagory:'Man',
+            price:50,
+            image:'https://i.ibb.co/wN9mH1v/ManShow1.jpg',
+            description:'Rockport Mens Eureka Walking Shoe'
+        }
+        ,
+        {
+            id=7,
+            name:'Woman collection',
+            catagory:'Woman',
+            price:30,
+            image:'https://i.ibb.co/5RgPxsv/p14.jpg',
+            description:'Alex Evenings Womens Plus Size Tea Length Dress with Rosette Detail '
+        }
+        ,
+        {
+            id=8,
+            name:'Woman Collection2',
+            catagory:'Woman',
+            price:10,
+            image:'https://i.ibb.co/0r4py5h/woman-Collection.jpg',
+            description:'Smartwool Diamond Peak Quilted Dress - Womens'
+        }
+        ,
+        {
+            id=9,
+            name:'Woman Pant collection',
+            catagory:'Woman',
+            price:40,
+            image:'https://i.ibb.co/Snk7Yfg/womanpants.jpg',
+            description:'Regular Fit Trouser Pant'
+        }
+        ,
+        {
+            id=10,
+            name:'Woman Watch',
+            catagory:'Woman',
+            price:50,
+            image:'https://i.ibb.co/vcqGpRg/33.jpg',
+            description:'Woman Watch COllection'
+        }
+        ,
+        {
+            id=11,
+            name:'woman black Sunglas',
+            catagory:'Woman',
+            price:10,
+            image:'https://i.ibb.co/16qcVP9/p16.jpg',
+            description:'woman black Sunglas Collection'
+        }
+        ,
+        {
+            id=12,
+            name:'woman  show',
+            catagory:'Woman',
+            price:50,
+            image:'https://i.ibb.co/7nPyTmj/womanshow.png',
+            description:' Woman Show Collection'
+        }
+        ,
+        {
+            id=13,
+            name:'woman  Bag collection',
+            catagory:'Woman',
+            price:30,
+            image:'https://i.ibb.co/Bs9mnK8/Woman-Bag.jpg',
+            description:' Exclusive Bang Collection'
+        }
+        ,
+        {
+            id=14,
+            name:'Kids TShirt',
+            catagory:'Kids',
+            price:10,
+            image:'https://i.ibb.co/LZrqQ7c/kinds1.jpg',
+            description:'Kids TShirt Collection'
+        }
+        ,
+        {
+            id=15,
+            name:'woman Pant collection',
+            catagory:'kids',
+            price:30,
+            image:'https://i.ibb.co/bsMgvY0/kidspant.jpg',
+            description:'The Childrens Place Baby Toddler Boys Stretch Skinny Chino Pants'
+        }
+        ,
+        {
+            id=16,
+            name:'Kids Watch',
+            catagory:'Kids',
+            price:30,
+            image:'https://i.ibb.co/Qvct3vr/kidswatch.jpg',
+            description:'Kids Watch Collection'
+        }
+        ,
+        {
+            id=17,
+            name:'Kids black Sunglas',
+            catagory:'Kids',
+            price:15,
+            image:'https://i.ibb.co/FDJwkdD/kindssunglass.jpg',
+            description:'Hipsterkid Polarized Wayfarers Kids, Baby, Toddler Sunglasses w/ Strap - UVA/UVB Protection, Shatter-Resistant Lenses'
+        }
+        ,
+        {
+            id=18,
+            name:'Kids  show',
+            catagory:'Kids',
+            price:50,
+            image:'https://i.ibb.co/kHzz1CX/kidsshow.jpg',
+            description:'Josmo Unisex-Child Walking Shoes First Walker'
+        },
+    ]
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FackData;
